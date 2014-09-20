756053e8dab2089b297390e3312f186cc9db1db5
Sämir Hamadi <samir.hamadi@elisa.fi>
2014-09-20 14:11:08 +0300

    Mocked note uses the queried userid

--- application/src/main/scala/fi/samssi/notesongo/service/MockedNoteService.scala
+++ application/src/main/scala/fi/samssi/notesongo/service/MockedNoteService.scala
@@ -3,7 +3,5 @@ package fi.samssi.notesongo.service
 import fi.samssi.notesongo.domain.Note
 
 class MockedNoteService extends NoteService {
-  val note = Note("foo@bar.com", "My mocked note", "Unfinished note...")
-
-  def findNoteFor(userId: String) = note
+  def findNoteFor(userId: String) = Note(userId, "My mocked note", "Unfinished note...")
 }
